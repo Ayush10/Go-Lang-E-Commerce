1f746ec3d745e22694a2ef3df06e3978d8081899
Ayush10 <ayushgreat10@gmail.com>
2022-04-06 23:38:58 +0545

    Started Working on Models.go File

--- models/models.go
+++ models/models.go
@@ -1,1 +1,18 @@
 package models
+
+type User struct {
+	ID
+	FirstName
+	LastName
+	Password
+	Email
+	Phone
+	Token
+	RefreshToken
+	CreatedAt
+	UpdatedAt
+	UserID
+	UserCart
+	AddressDetails
+	OrderStatus
+}
